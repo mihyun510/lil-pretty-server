a76a40c9b578a46ab4d1fa3fac03a577f8ba12b8
kmh <mihyun510@github.com>
2025-05-05 16:11:32 +0900

    first commit

---
+++ src/main/java/com/lil/pretty/LilPerttyApplication.java
@@ -0,0 +1,13 @@
+package com.lil.pretty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilPerttyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LilPerttyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lil/pretty/LilPerttyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lil.pretty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilPerttyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
