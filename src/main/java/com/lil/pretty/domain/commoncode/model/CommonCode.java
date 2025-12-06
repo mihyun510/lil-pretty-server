a3b8c91253d51ff00209ca352665957f9deb6cb4
kmh <mihyun510@github.com>
2025-12-06 15:08:22 +0900

    common code commit

--- src/main/java/com/lil/pretty/domain/commoncode/model/CommonCode.java
+++ src/main/java/com/lil/pretty/domain/commoncode/model/CommonCode.java
@@ -1,5 +1,35 @@
 package com.lil.pretty.domain.commoncode.model;
 
-public class CommonCode {
+import com.lil.pretty.domain.common.model.BaseEntity;
+import com.lil.pretty.domain.diet.model.MealMst;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "commoncode")
+public class CommonCode extends BaseEntity{
+	@Id
+    @Column(name = "cm_grp_cd", length = 5, nullable = false)
+    private String cmGrpCd;   
+
+    @Column(name = "cm_dt_cd", length = 5, nullable = false)
+    private String cmDtCd;  
+
+    @Column(name = "cm_grp_nm", length = 100)
+    private String cmGrpNm;  
+
+    @Column(name = "cm_grp_desc", length = 1000)
+    private String cmGrpDesc;  
+
+    @Column(name = "cm_dt_nm", length = 100)
+    private String cmDtNm;
+
+    @Column(name = "cm_dt_desc", length = 1000)
+    private int cmDtDesc;  
+    
 }
