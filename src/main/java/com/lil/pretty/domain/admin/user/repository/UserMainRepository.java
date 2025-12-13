d036bf6f1f136af7e09f614649951b217917bfa6
mj <minjeong1513@naver.com>
2025-12-14 01:43:06 +0900

    12/13 사용자권한관리 조회,수정,삭제 서비스

--- src/main/java/com/lil/pretty/domain/admin/user/repository/UserMainRepository.java
+++ src/main/java/com/lil/pretty/domain/admin/user/repository/UserMainRepository.java
@@ -3,6 +3,7 @@ package com.lil.pretty.domain.admin.user.repository;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -12,7 +13,36 @@ import com.lil.pretty.domain.user.model.User;
 public interface UserMainRepository extends CrudRepository <User, String> {
 	@Query(value = "SELECT *  \n"
 			+ " FROM user  \n"
-		    + " WHERE us_id = :usId; ", nativeQuery = true)
+		    + " WHERE :usId IS NULL OR :usId = '' OR us_id = :usId; ", nativeQuery = true)
   
 	List<Map<String,Object>> findAdminUserItems(@Param("usId") String usId);
+
+	//사용자 정보 삭제
+	@Modifying
+	@Query(value = "DELETE FROM  user  \n"
+			    + " WHERE us_id = :usId ", nativeQuery = true)
+	  
+		int deleteAdminUserItems(@Param("usId") String usId);
+	
+	//사용자 정보 수정
+	@Modifying
+	@Query(value = "UPDATE user  \n"
+			+ " SET \n"
+			+ "  us_pw = :usPw \n"
+			+ " , us_nm = :usNm \n"
+			+ " , us_email = :usEmail \n"
+			+ " , us_phone = :usPhone \n"
+			+ " , us_role = :usRole \n"
+			+ " , us_img = :usImg \n"
+			+ " , upd_date = NOW() \n"
+			+ " , upd_user = :usId \n"
+		    + " WHERE us_id = :usId; ", nativeQuery = true)
+  
+	int updateAdminUserItems(@Param("usId") String usId, 
+	        @Param("usPw") String usPw,
+	        @Param("usNm") String usNm, 
+	        @Param("usEmail") String usEmail,
+	        @Param("usPhone") String usPhone,
+	        @Param("usRole") String usRole,
+	        @Param("usImg") String usImg);
 }
--- src/main/java/com/lil/pretty/domain/admin/user/service/UserMainService.java
+++ src/main/java/com/lil/pretty/domain/admin/user/service/UserMainService.java
@@ -6,13 +6,25 @@ import org.springframework.stereotype.Service;
 import com.lil.pretty.domain.admin.user.repository.UserMainRepository;
 import com.lil.pretty.domain.user.model.User;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class UserMainService {
 private final UserMainRepository userMainRepository;
+	
 	public List<Map<String,Object>> getAdminUserItems(String usId){
 		return userMainRepository.findAdminUserItems(usId);
+	}
+	@Transactional
+	public int deleteAdminUserItems(String usId){
+		System.out.print("민정 11삭제 테스트 usid"+usId);
+		return userMainRepository.deleteAdminUserItems(usId);
+	}
+	@Transactional
+	public int updateAdminUserItems(String usId, String usPw, String usNm, String usEmail, String usPhone, String usRole, String usImg){
+		
+		return userMainRepository.updateAdminUserItems(usId, usPw, usNm, usEmail, usPhone, usRole, usImg);
 	}
 }
